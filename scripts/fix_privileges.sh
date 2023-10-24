204358cee41157ff21952d2cb41fe307a44263ce
djmeier-wisc <71796515+djmeier-wisc@users.noreply.github.com>
2023-10-23 21:51:35 -0500

    fix chmod issue

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -2,3 +2,5 @@
 chmod +x /home/ec2-user/server/target/*.jar
 chmod +x /home/ec2-user/server/scripts/server_start.sh
 chmod +x /home/ec2-user/server/scripts/server_stop.sh
+chmod +x /home/ec2-user/server/scripts/server_validate.sh
+chmod +x /home/ec2-user/server/scripts/server_clear.sh
