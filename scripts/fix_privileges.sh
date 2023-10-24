aae3ccba739c0d3069f0f99a6eb700d53fcb346f
djmeier-wisc <71796515+djmeier-wisc@users.noreply.github.com>
2023-10-23 21:43:53 -0500

    update to keep paths

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+chmod +x /home/ec2-user/server/target/*.jar
+chmod +x /home/ec2-user/server/scripts/server_start.sh
+chmod +x /home/ec2-user/server/scripts/server_stop.sh
