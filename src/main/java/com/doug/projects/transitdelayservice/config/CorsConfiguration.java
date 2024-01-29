62643d9e42c027b4adf2066bd6eddff872e77ba4
djmeier-wisc <djmeier@wisc.edu>
2024-01-29 12:48:13 -0600

    check multiple urls, ensure they are in expected list

--- src/main/java/com/doug/projects/transitdelayservice/config/CorsConfiguration.java
+++ src/main/java/com/doug/projects/transitdelayservice/config/CorsConfiguration.java
@@ -18,8 +18,8 @@ public class CorsConfiguration {
     private static final String ALLOWED_HEADERS =
             "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
-    private static final String ALLOWED_ORIGIN =
-            "https://my-precious-time.com, https://www.my-precious-time.com, https://api.my-precious-time.com, http://localhost";
+    private static final String[] ALLOWED_ORIGIN =
+            new String[]{"my-precious-time.com", "localhost"};
     private static final String MAX_AGE = "3600";
 
     @Bean
@@ -28,10 +28,15 @@ public class CorsConfiguration {
             ServerHttpRequest request = ctx.getRequest();
             ServerHttpResponse response = ctx.getResponse();
             HttpHeaders headers = response.getHeaders();
-            headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-            headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-            headers.add("Access-Control-Max-Age", MAX_AGE);
-            headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+            for (var origin : ALLOWED_ORIGIN) {
+                if (request.getURI().getHost().contains(origin)) {
+                    headers.add("Access-Control-Allow-Origin", origin);
+                    headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+                    headers.add("Access-Control-Max-Age", MAX_AGE);
+                    headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+                }
+            }
+
             if (request.getMethod() == HttpMethod.OPTIONS) {
                 response.setStatusCode(HttpStatus.OK);
                 return Mono.empty();
