ec4b72ea9c62e48ab2ebbae9c57e841e3fc2e5c7
djmeier-wisc <djmeier@wisc.edu>
2024-01-29 12:18:30 -0600

    add cors configuration

---
+++ src/main/java/com/doug/projects/transitdelayservice/config/CorsConfiguration.java
@@ -0,0 +1,43 @@
+package com.doug.projects.transitdelayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CorsConfiguration {
+
+    private static final String ALLOWED_HEADERS =
+            "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
+    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
+    private static final String ALLOWED_ORIGIN =
+            "https://my-precious-time.com, https://www.my-precious-time.com, https://api.my-precious-time.com";
+    private static final String MAX_AGE = "3600";
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            ServerHttpResponse response = ctx.getResponse();
+            HttpHeaders headers = response.getHeaders();
+            headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+            headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+            headers.add("Access-Control-Max-Age", MAX_AGE);
+            headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                response.setStatusCode(HttpStatus.OK);
+                return Mono.empty();
+            }
+            return chain.filter(ctx);
+        };
+    }
+
+}
--- src/main/java/com/doug/projects/transitdelayservice/controller/HealthController.java
+++ src/main/java/com/doug/projects/transitdelayservice/controller/HealthController.java
@@ -1,12 +1,10 @@
 package com.doug.projects.transitdelayservice.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "www.my-precious-time.com"})
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> health() {
