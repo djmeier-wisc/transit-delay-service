40b63e8776b96b28ce713ed3ca99b865ac310511
djmeier-wisc <djmeier@wisc.edu>
2024-04-01 21:03:30 -0500

    remove unused feedId

--- src/main/java/com/doug/projects/transitdelayservice/entity/GraphOptions.java
+++ src/main/java/com/doug/projects/transitdelayservice/entity/GraphOptions.java
@@ -20,7 +20,6 @@ public class GraphOptions {
     private Boolean useColor;
     //the routes to query the db for
     private List<String> routes;
-    private String feedId;
     private Integer lowerOnTimeThreshold;
     private Integer upperOnTimeThreshold;
 }
