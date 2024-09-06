e5c38cde7c03942d8d6ff9f3e1777157f71ae2e2
djmeier-wisc <djmeier@wisc.edu>
2024-09-05 22:33:48 -0500

    BUGFIX: update yum to use -y
    Reduce number of threads available for static polling

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo yum install java-17-amazon-corretto
+sudo yum -y install java-17-amazon-corretto
 sudo java -jar target/*.jar > logs.txt 2> logs.txt < logs.txt &
--- src/main/java/com/doug/projects/transitdelayservice/TransitDelayServiceApplication.java
+++ src/main/java/com/doug/projects/transitdelayservice/TransitDelayServiceApplication.java
@@ -54,12 +54,12 @@ public class TransitDelayServiceApplication {
 
     @Bean("realtime")
     public Executor realtimeExecutor() {
-        return Executors.newFixedThreadPool(4);
+        return Executors.newFixedThreadPool(1);
     }
 
     @Bean("retry")
     public Executor retryExecutor() {
-        return Executors.newFixedThreadPool(4);
+        return Executors.newFixedThreadPool(1);
     }
 
     @Bean("dynamoWriting")
